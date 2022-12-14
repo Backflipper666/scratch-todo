41c1c166ee48e76289d13bf1bfadac89fec9c975
Backflipper666 <elderly619@gmail.com>
2022-12-14 12:45:15 +0600

    change NewTaskForm from class component to functional

--- src/components/NewTaskForm/NewTaskForm.js
+++ src/components/NewTaskForm/NewTaskForm.js
@@ -1,90 +1,66 @@
-import React from 'react'
+import { useState } from 'react'
 
-class NewTaskForm extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      label: '',
-      minutes: '',
-      seconds: '',
-    }
-    this.statusHandler = this.statusHandler.bind(this)
-    this.onHitEnter = this.onHitEnter.bind(this)
-    this.onChangeSec = this.onChangeSec.bind(this)
-    this.onLongArrowChange = this.onLongArrowChange.bind(this)
-  }
-  statusHandler(e) {
-    console.log(e)
-  }
+const NewTaskForm = ({ inputTextHandler, inputText, onFormSubmit, onKeyUpInputCopy }) => {
+  const [seconds, setSeconds] = useState('')
+  const [minutes, setMinutes] = useState('')
 
-  onHitEnter(e) {
+  const onHitEnter = (e) => {
     if (!e.target.value.trim().length) return
-    if (!this.state.minutes.trim().length && !this.state.seconds.trim().length) return
+    if (!minutes.trim().length && !seconds.trim().length) return
 
     if (e.key === 'Enter') {
-      if (isNaN(Number(this.state.minutes)) || isNaN(Number(this.state.seconds))) {
+      if (isNaN(Number(minutes)) || isNaN(Number(seconds))) {
         return
       }
-      this.props.onKeyUpInputCopy(e, this.state.minutes, this.state.seconds)
-      this.setState({
-        minutes: '',
-        seconds: '',
-      })
+      onKeyUpInputCopy(e, minutes, seconds)
+      setSeconds('')
+      setMinutes('')
     }
   }
 
-  onChangeSec(e) {
-    this.setState({
-      seconds: e.target.value,
-    })
-  }
-  onLongArrowChange(e) {
-    this.setState({
-      minutes: e.target.value,
-    })
+  const onChangeSec = (e) => {
+    setSeconds(e.target.value)
   }
 
-  render() {
-    // const onKeyUpInput = this.props.onKeyUpInput
-    const inputTextHandler = this.props.inputTextHandler
-    const inputText = this.props.inputText
-    const onFormSubmit = this.props.onFormSubmit
-    return (
-      <header className="header">
-        <h1>todos</h1>
-        <form className="new-todo-form" onSubmit={onFormSubmit}>
-          <input
-            className="new-todo"
-            placeholder="Task"
-            value={inputText}
-            onChange={inputTextHandler}
-            onKeyUp={this.onHitEnter}
-            autoFocus
-          />
-          <input
-            value={this.state.minutes}
-            onChange={this.onLongArrowChange}
-            name="min"
-            max="60"
-            min="0"
-            type="text"
-            className="new-todo-form__timer"
-            placeholder="Min"
-          />
-          <input
-            value={this.state.seconds}
-            onChange={this.onChangeSec}
-            name="sec"
-            max="60"
-            min="0"
-            type="text"
-            className="new-todo-form__timer"
-            placeholder="Sec"
-          />
-        </form>
-      </header>
-    )
+  const onLongArrowChange = (e) => {
+    setMinutes(e.target.value)
   }
+
+  return (
+    <header className="header">
+      <h1>todos</h1>
+      <form className="new-todo-form" onSubmit={onFormSubmit}>
+        <input
+          className="new-todo"
+          placeholder="Task"
+          value={inputText}
+          onChange={inputTextHandler}
+          onKeyUp={onHitEnter}
+          autoFocus
+        />
+        <input
+          value={minutes}
+          onChange={onLongArrowChange}
+          name="min"
+          max="60"
+          min="0"
+          type="text"
+          className="new-todo-form__timer"
+          placeholder="Min"
+        />
+        <input
+          value={seconds}
+          onChange={onChangeSec}
+          name="sec"
+          max="60"
+          min="0"
+          type="text"
+          className="new-todo-form__timer"
+          placeholder="Sec"
+        />
+      </form>
+    </header>
+  )
 }
 
 export default NewTaskForm
